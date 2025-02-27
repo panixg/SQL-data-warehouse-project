a35ae7ac8141fd069f645eb4b605dcab07df5532
panixg <leon.pualp@gmail.com>
2025-02-26 23:07:48 -0300

    bulk insert bronze

--- scripts/Bulk Insert Bronze.sql
+++ scripts/Bulk Insert Bronze.sql
@@ -1,3 +1,4 @@
+TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT bronze.crm_cust_info
 FROM 'C:.../crm_cust_info.csv'
 WITH
@@ -7,3 +8,59 @@ WITH
     FIRSTROW = 2,
     TABLOCK
 );
+
+TRUNCATE TABLE bronze.crm_prd_info;
+BULK INSERT bronze.crm_prd_info
+FROM 'C:.../crm_prd_info.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+FROM 'C:.../crm_sales_details.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+    TABLOCK
+);
+
+TRUNCATE Table bronze.erp_cust_az12;
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:.../erp_cust_az12.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+    TABLOCK
+);
+
+TRUNCATE Table bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:.../erp_loc_a101.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+    TABLOCK
+);
+
+TRUNCATE Table bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:.../erp_px_cat_g1v2.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    FIRSTROW = 2,
+    TABLOCK
+);
+
