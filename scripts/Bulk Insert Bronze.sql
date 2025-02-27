01e43cb9e7834b03a200ecc1092ae945406cfdd0
panixg <leon.pualp@gmail.com>
2025-02-27 00:40:37 -0300

    ddl bronze y bulk insert bronze

--- scripts/Bulk Insert Bronze.sql
+++ scripts/Bulk Insert Bronze.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT bronze.crm_cust_info
-FROM 'C:.../crm_cust_info.csv'
+FROM 'C:...\cust_info.csv'
 WITH
 (
     FIELDTERMINATOR = ',',
@@ -11,7 +11,7 @@ WITH
 
 TRUNCATE TABLE bronze.crm_prd_info;
 BULK INSERT bronze.crm_prd_info
-FROM 'C:.../crm_prd_info.csv'
+FROM 'C:...\prd_info.csv'
 WITH
 (
     FIELDTERMINATOR = ',',
@@ -22,7 +22,7 @@ WITH
 
 TRUNCATE TABLE bronze.crm_sales_details;
 BULK INSERT bronze.crm_sales_details
-FROM 'C:.../crm_sales_details.csv'
+FROM 'C:...\sales_details.csv'
 WITH
 (
     FIELDTERMINATOR = ',',
@@ -33,7 +33,7 @@ WITH
 
 TRUNCATE Table bronze.erp_cust_az12;
 BULK INSERT bronze.erp_cust_az12
-FROM 'C:.../erp_cust_az12.csv'
+FROM 'C:...\cust_az12.csv'
 WITH
 (
     FIELDTERMINATOR = ',',
@@ -44,7 +44,7 @@ WITH
 
 TRUNCATE Table bronze.erp_loc_a101;
 BULK INSERT bronze.erp_loc_a101
-FROM 'C:.../erp_loc_a101.csv'
+FROM 'C:...\loc_a101.csv'
 WITH
 (
     FIELDTERMINATOR = ',',
@@ -55,7 +55,7 @@ WITH
 
 TRUNCATE Table bronze.erp_px_cat_g1v2;
 BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:.../erp_px_cat_g1v2.csv'
+FROM 'C:...\px_cat_g1v2.csv'
 WITH
 (
     FIELDTERMINATOR = ',',
